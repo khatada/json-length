1f38b8415627ee5ac3994b33c1198bfb08eb5f26
hatada koki <koki.hatada@gmail.com>
2018-04-23 13:07:27 +0900

    cosme

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 "use strict"
 
-function __jsonLength(json, visited){
-    if(typeof json === "undefined"){
+function __jsonLength(json, visited) {
+    if (typeof json === "undefined") {
         return 0;
-    }else if(json === null){
+    } else if (json === null) {
         return 4; // null
-    }else if(typeof json === "number"){
+    } else if (typeof json === "number") {
         return String(json).length;
-    }else if(typeof json === "string" || json instanceof String){
+    } else if (typeof json === "string" || json instanceof String) {
         return json.length + 2;
-    }else if(json instanceof Date){
+    } else if (json instanceof Date) {
         return json.toISOString().length + 2;
-    }else if(json instanceof RegExp){
+    } else if (json instanceof RegExp) {
         return 0;
-    }else if(typeof json === "function"){
+    } else if (typeof json === "function") {
         return 0;
-    }else if(typeof json === "array" || Array.isArray(json)){
-        if(visited.length && visited.indexOf(json) >= 0){
+    } else if (typeof json === "array" || Array.isArray(json)) {
+        if (visited.length && visited.indexOf(json) >= 0) {
             throw new Error("Counting text length of JSON of circular structure");
         }
 
         visited.push(json);
         let sum = 0;
         const count = json.length;
-        for(let i=0 ; i<count ; i++){
+        for (let i = 0; i < count; i++) {
             const element = json[i];
             const length = __jsonLength(element, visited);
-            // console.log(length, element)
-            if(length > 0){
+            if (length > 0) {
                 sum += length + 1; // add comma
-            }else{
+            } else {
                 sum += 5 // null,
             }
         }
 
-        if(count > 1){
+        if (count > 1) {
             return sum + 1; // [ ... ] with removing trailing comma
-        }else{
+        } else {
             return sum + 2; // [ ... ]
         }
-        
-    }else if(typeof json === "object"){
-        if(visited.length && visited.indexOf(json) >= 0){
+
+    } else if (typeof json === "object") {
+        if (visited.length && visited.indexOf(json) >= 0) {
             throw new Error("Counting text length of JSON of circular structure");
         }
 
@@ -49,30 +48,27 @@ function __jsonLength(json, visited){
         let sum = 0;
         let count = 0;
         const keys = Object.keys(json);
-        for(let i=0 ; i<keys.length ; i++){
+        for (let i = 0; i < keys.length; i++) {
             const key = keys[i];
             const length = __jsonLength(json[key], visited);
-            // console.log(length, key , property)
-            if(length > 0){
+            if (length > 0) {
                 count++;
                 sum += key.length + length + 4; // "..": ...,
             }
         };
-        
-        if(count > 0){
+
+        if (count > 0) {
             return sum + 1; // { ... } with removing trailing comma
-        }else{
+        } else {
             return sum + 2; // { ... }
         }
-        
-    }else{
-        console.log("error does not match any type ", json);
+
+    } else {
         return 0;
     }
 }
 
-function jsonLength(json){
-    // const visited = new Set();
+function jsonLength(json) {
     return __jsonLength(json, []);
 }
 
